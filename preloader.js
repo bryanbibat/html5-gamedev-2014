1d0f0f659cef1b5367a3093657ba95aae8ca408d
Bryan Bibat <bry@bryanbibat.net>
2014-05-24 02:23:10 +0800

    New Template

--- preloader.js
+++ preloader.js
@@ -25,8 +25,8 @@ BasicGame.Preloader.prototype = {
     //  Here we load the rest of the assets our game needs.
     this.load.image('titlepage', 'assets/titlepage.png');
     this.load.image('sea', 'assets/sea.png');
-    this.load.image('bulletSmall', 'assets/bullet-small.png');
-    this.load.image('enemyBulletSmall', 'assets/enemy-bullet-small.png');
+    this.load.image('bullet', 'assets/bullet.png');
+    this.load.image('enemyBullet', 'assets/enemy-bullet.png');
     this.load.spritesheet('greenEnemy', 'assets/enemy.png', 32, 32);
     this.load.spritesheet('whiteEnemy', 'assets/shooting-enemy.png', 32, 32);
     this.load.spritesheet('boss', 'assets/boss.png', 93, 75);
